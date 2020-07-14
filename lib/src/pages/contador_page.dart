e415104bd8eb9dd3d46e0083dc53cd7e497602df
Carlos Carrasco Rodriguez <carlos.carrasco@gmail.com>
2020-07-14 16:56:42 -0400

    agregar botones mas menos y cero

--- lib/src/pages/contador_page.dart
+++ lib/src/pages/contador_page.dart
@@ -29,15 +29,35 @@ class _ContadorPageState extends State<ContadorPage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            _conteo++;
-          });
-        },
-      ),
+      floatingActionButton: _crearBontones()
     );
   }
-}
 
+  Widget _crearBontones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width:30),
+        FloatingActionButton(child: Icon(Icons.exposure_zero), onPressed: _resetContador),
+        Expanded(child: SizedBox(),), 
+        FloatingActionButton(child: Icon(Icons.remove), onPressed: _restart),
+        SizedBox(width: 10.0 ),
+        FloatingActionButton(child: Icon(Icons.add), onPressed: _agregar),
+
+      ],
+    );
+  } // _crearBotones
+
+  void _agregar(){
+    setState(() => _conteo++);
+  }
+
+  void _resetContador(){
+    setState(() => _conteo=0);
+  }
+
+    void _restart(){
+    setState(() => _conteo--);
+  }
+
+}
